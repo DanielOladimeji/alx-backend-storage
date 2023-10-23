46ade13b2b16106d57946fb7c037ea1b3b836dfc
DanielOladimeji <danieloladimejiblessing@gmail.com>
2023-10-23 01:36:25 -0700

    done

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,21 @@
 #!/usr/bin/env python3
+"""get_page function using the requests module to fetch the
+content of a URL and cache it using a decorator.
 """
-web cache and tracker
-"""
-import requests
 import redis
-from functools import wraps
-
-store = redis.Redis()
-
-
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
-    return wrapper
+import requests
+r = redis.Redis()
+count = 0
 
 
-@count_url_access
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """Define the key for caching."""
+    r.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    r.incr(f"count:{url}")
+    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    return resp.text
+
+
+if __name__ == "__main__":
+    get_page("http://slowwly.robertomurray.co.uk")
